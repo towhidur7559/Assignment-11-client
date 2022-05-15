f8a859333a23536c25478614d31ed831e77b5dd2
Safinxr <safinxr@gmail.com>
2022-05-15 23:29:57 +0600

    navbar hide

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,7 +1,14 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, NavLink } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const logout = () => {
+    signOut(auth);
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light shadow-lg bg-white p-1 sticky-top">
       <div className="container">
@@ -33,7 +40,7 @@ const Navbar = () => {
                 Home
               </NavLink>
             </li>
-            <li className="nave-item">
+            <li className={user?'nave-item':'d-none'}>
               <NavLink
                 className={({ isActive }) =>
                   isActive ? "nav-link text-info fs-5 ms-4" : "nav-link fs-5 ms-4"
@@ -43,7 +50,7 @@ const Navbar = () => {
                 Manage items
               </NavLink>
             </li>
-            <li className="nave-item">
+            <li className={user?'nave-item':'d-none'}>
               <NavLink
                 className={({ isActive }) =>
                   isActive ? "nav-link text-info fs-5 ms-4" : "nav-link fs-5 ms-4"
@@ -53,7 +60,7 @@ const Navbar = () => {
                 Add items
               </NavLink>
             </li>
-            <li className="nave-item">
+            <li className={user?'nave-item':'d-none'}>
               <NavLink
                 className={({ isActive }) =>
                   isActive ? "nav-link text-info fs-5 ms-4" : "nav-link fs-5 ms-4"
@@ -74,14 +81,23 @@ const Navbar = () => {
               </NavLink>
             </li>
             <li className="nave-item">
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? "nav-link text-info fs-5 ms-4" : "nav-link fs-5 ms-4"
-                }
-                to="/login"
-              >
-                Login
-              </NavLink>
+            {
+              user? <NavLink onClick={logout}
+              className={({ isActive }) =>
+                isActive ? "nav-link text-info fw-bold fs-5 ms-4" : "nav-link fs-5 ms-4"
+              }
+              to="/login"
+            >
+              Sign out
+            </NavLink>:<NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-link text-info fs-5 ms-4" : "nav-link fs-5 ms-4"
+              }
+              to="/login"
+            >
+              Login
+            </NavLink>
+            }
             </li>
           </ul>
         </div>
